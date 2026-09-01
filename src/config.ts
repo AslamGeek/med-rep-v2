ddccb19f865c2fc6d43a561b48b2f33264d73c3f
Aslamz <aslamzblogger@gmail.com>
2026-09-01 23:52:35 +0530

    as

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const GAS_WEBAPP_URL =
   import.meta.env.VITE_GAS_WEBAPP_URL ||
-  'https://script.google.com/macros/s/AKfycbx7QgUgZ_xNy1sPspQRfEQjmjxhM-pv4HWA-q_RrQH_wWb28TdxEuJjWthWXv65EdHO/exec'
-
+  'https://script.google.com/macros/s/AKfycby2OoWvEeBflbSuJbuR-ZBCcqCi4Y1A1gOwHlVdHQFCoQ41IpF-1wD0X4BS_myQUjc4/exec'
+  
 export const APP_NAME = 'MedRep'
