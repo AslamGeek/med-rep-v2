c30e1104ecfa90249751b6c7ea178ece97fc32e2
Aslamz <aslamzblogger@gmail.com>
2026-09-02 10:00:27 +0530

    xas

--- src/components/FilterSheet.tsx
+++ src/components/FilterSheet.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { X } from 'lucide-react'
-import type { Prescriber, SavedFilters } from '../types'
+import type { SavedFilters } from '../types'
 
 type Draft = Pick
   SavedFilters,
